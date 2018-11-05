ab457303a0c3284dcebd0f1e34a82766187a3a21
skluzada <38167459+skluzada@users.noreply.github.com>
2018-11-05 11:24:18 +0100

    Added tests for the 4th assignment

--- src/suite02/TroopTileTest.java
+++ src/suite02/TroopTileTest.java
@@ -9,9 +9,8 @@ import java.lang.reflect.Modifier;
 
 import org.junit.Test;
 
-import WWW.thedrake.Offset2D;
 import WWW.thedrake.PlayingSide;
-import WWW.thedrake.Troop;
+import WWW.thedrake.StandardDrakeSetup;
 import WWW.thedrake.TroopFace;
 import WWW.thedrake.TroopTile;
 
@@ -28,11 +27,10 @@ public class TroopTileTest {
 	
 	@Test
 	public void behaviour() {
-		Troop monk = new Troop("Monk", new Offset2D(1, 1));
-		Troop drake = new Troop("Drake");
+		StandardDrakeSetup setup = new StandardDrakeSetup();
 		
-		TroopTile tile1 = new TroopTile(monk, PlayingSide.BLUE, TroopFace.AVERS);
-		TroopTile tile2 = new TroopTile(drake, PlayingSide.ORANGE, TroopFace.REVERS);
+		TroopTile tile1 = new TroopTile(setup.MONK, PlayingSide.BLUE, TroopFace.AVERS);
+		TroopTile tile2 = new TroopTile(setup.DRAKE, PlayingSide.ORANGE, TroopFace.REVERS);
 		
 		assertFalse(tile1.canStepOn());
 		assertFalse(tile2.canStepOn());
@@ -40,8 +38,8 @@ public class TroopTileTest {
 		assertTrue(tile1.hasTroop());
 		assertTrue(tile2.hasTroop());
 			
-		assertSame(monk, tile1.troop());
-		assertSame(drake, tile2.troop());
+		assertSame(setup.MONK, tile1.troop());
+		assertSame(setup.DRAKE, tile2.troop());
 		
 		assertSame(PlayingSide.BLUE, tile1.side());
 		assertSame(PlayingSide.ORANGE, tile2.side());
